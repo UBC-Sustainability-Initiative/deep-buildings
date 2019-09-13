ba476297ffe23e0abe32680c16179072fe8e8d3c
rohannuttall <r.nuttall@alumni.ubc.ca>
2019-09-13 11:37:15 -0700

    saving all data

--- src/modeling/GPC/get_data.py
+++ src/modeling/GPC/get_data.py
@@ -322,14 +322,14 @@ def split_train_test(X, y, pct_train=0.8, month_range=None, test_year=None,
         else: 
             pname = '.../data/processed/GPC/'
         
-        train = trainX.copy()
-        test = testX.copy()
-        train['trainY'] = trainY
-        test['testY'] = testY
-        with open(pname+'train.pkl', 'wb') as outfile:
-                pickle.dump(train, outfile, pickle.HIGHEST_PROTOCOL)
-        with open(pname+'test.pkl', 'wb') as outfile:
-                pickle.dump(test, outfile, pickle.HIGHEST_PROTOCOL)
-            
+        with open(pname+'trainX.pkl', 'wb') as outfile:
+                pickle.dump(trainX, outfile, pickle.HIGHEST_PROTOCOL)
+        with open(pname+'testX.pkl', 'wb') as outfile:
+                pickle.dump(testX, outfile, pickle.HIGHEST_PROTOCOL)
+        with open(pname+'trainY.pkl', 'wb') as outfile:
+                pickle.dump(trainY, outfile, pickle.HIGHEST_PROTOCOL)
+        with open(pname+'testY.pkl', 'wb') as outfile:
+                pickle.dump(testY, outfile, pickle.HIGHEST_PROTOCOL)
+                     
     return trainX, trainY, testX, testY
 
