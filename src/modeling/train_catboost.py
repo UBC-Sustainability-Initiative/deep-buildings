259eee644a077291986a7315084301e242d9421d
rohannuttall <r.nuttall@alumni.ubc.ca>
2019-09-18 12:16:24 -0700

    added frequency option

--- src/modeling/train_catboost.py
+++ src/modeling/train_catboost.py
@@ -9,6 +9,7 @@ from data.preprocess import read_processed_data
 @click.command()
 @click.argument('input_file', type=click.Path(exists=True, readable=True, dir_okay=False))
 @click.argument('output_file', type=click.Path(writable=True, dir_okay=False))
+@click.option('--frequency', default = 'D', show_default=True)
 @click.option('--scenario', default = 2, show_default=True)
 @click.option('--regr_vars', default = ['solar_radiation','temp','wind_dir',
                                         'hum_ratio','windspeed','weekday',
@@ -24,12 +25,12 @@ from data.preprocess import read_processed_data
 
 @click.option('--test_year', default = 2017, show_default=True)
 
-def main(input_file, output_file, scenario, regr_vars, multiplier, baseline,
+def main(input_file, output_file, frequency, scenario, regr_vars, multiplier, baseline,
          look_back, look_ahead, test_year):
     
     df = read_processed_data(input_file)
     
-    X, y= generate_data(df, freq='D', scenario=scenario, regr_vars = regr_vars,
+    X, y= generate_data(df, freq=frequency, scenario=scenario, regr_vars = regr_vars,
                         multiplier = multiplier, baseline= baseline, 
                         look_back = look_back, look_ahead = look_ahead)
     
